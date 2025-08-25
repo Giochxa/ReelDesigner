b92d903b6aaf2c9caf4ac166334b9128c432b5cc
Giochxa <151730615+Giochxa@users.noreply.github.com>
2025-08-25 15:10:02 +0400

    Update ReelModel.cs

--- Models/ReelModel.cs
+++ Models/ReelModel.cs
@@ -6,27 +6,26 @@ namespace ReelDesigner.Models
     {
         [Display(Name = "ბარაბნის დიამეტრი (mm)")]
         [Range(50, 5000)]
-        public double FlangeDiameter { get; set; } = 800;
+        public double FlangeDiameter { get; set; } = 1400;
 
         [Display(Name = "გულის დიამეტრი (mm)")]
         [Range(10, 4999)]
-        public double BarrelDiameter { get; set; } = 300;
+        public double BarrelDiameter { get; set; } = 700;
 
         [Display(Name = "შიდა სიგანე (mm)")]
         [Range(10, 5000)]
-        public double Width { get; set; } = 600;
+        public double Width { get; set; } = 880;
 
         [Display(Name = "ნახვრეტის დიამეტრი (mm)")]
         [Range(5, 500)]
-        public double ArborHoleDiameter { get; set; } = 50;
+        public double ArborHoleDiameter { get; set; } = 100;
 
         [Display(Name = "გვერდის სისქე (mm)")]
         [Range(5, 100)]
-        public double FlangeThickness { get; set; } = 30;
+        public double FlangeThickness { get; set; } = 60;
 
         [Display(Name = "გულის სისქე (mm)")]
         [Range(5, 200)]
-        public double DrumThickness { get; set; } = 50;
+        public double DrumThickness { get; set; } = 30;
     }
 }
-
